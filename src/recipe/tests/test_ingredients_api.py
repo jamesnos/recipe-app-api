f6d9c28f4ac9ffc68974c8ce98a3c95f784caa4e
James Nos <jimmytnos@gmail.com>
2020-01-26 14:10:09 +0800

    Working ingredients api

--- src/recipe/tests/test_ingredients_api.py
+++ src/recipe/tests/test_ingredients_api.py
@@ -56,7 +56,7 @@ class PrivateIngredientsApiTests(TestCase):
             'otherpasword'
         )
         Ingredient.objects.create(user=user2, name='Cucumber')
-        
+
         ingredient = Ingredient.objects.create(user=self.user, name='Kale')
 
         res = self.client.get(INGREDIENTS_URL)
@@ -64,3 +64,22 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """Test creating a new ingredient"""
+        payload = {
+            'name': 'Cucuber'
+        }
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
